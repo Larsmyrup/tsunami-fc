ee20879c7600bc5cf5c896260c7889f33237f933
larsmyrup <hej@larsmyrup.dk>
2020-12-04 13:58:28 +0100

    Experimenting with sounds

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,57 @@
-<template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
-</template>
-
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import ClickSound from './assets/click-sound.mp3'
+import ClickSound2 from './assets/click-sound.mp3'
+import ClickSound3 from './assets/tsunami-logo.mp3'
+
+const clickSound = new Audio(ClickSound)
+const clickSound2 = new Audio(ClickSound2)
+const clickSound3 = new Audio(ClickSound3)
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+  data() {
+    return {
+      counter: 1,
+    }
+  },
+  methods: {
+    onLogoClick() {
+      if (this.counter % 2 === 0 || this.counter % 7 === 0) {
+        console.log('lyd 2')
+        clickSound.play();
+      } else {
+        console.log('lyd 1')
+
+        clickSound2.play();
+      }
+      this.counter++;
+
+      if (this.counter > 10) {
+        this.playLoop();
+      }
+    },
+    playLoop() {
+      this.playTsunami().then(() => {
+        this.playLoop();
+      })
+    },
+    playTsunami() {
+      return clickSound3.play();
+    },
+  },
 }
 </script>
 
-<style>
+<template>
+  <div id="app" class="app">
+    <img
+        class="app__logo"
+        src="./assets/tsunami.jpg"
+        @click="onLogoClick"
+    >
+  </div>
+</template>
+
+<style lang="scss">
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -24,5 +59,19 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+.app {
+  &__logo {
+    cursor: pointer;
+
+    &:hover {
+      filter: brightness(102%);
+    }
+
+    &:active {
+      filter: brightness(105%);
+    }
+  }
 }
 </style>
