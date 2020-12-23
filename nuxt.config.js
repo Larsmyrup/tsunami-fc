5f64179c8a17959273928612b230138a561b4383
larsmyrup <hej@larsmyrup.dk>
2020-12-23 20:26:39 +0100

    404 page should work

--- nuxt.config.js
+++ nuxt.config.js
@@ -11,6 +11,14 @@ export default {
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
     ],
   },
+  router: {
+    extendRoutes(routes, resolve) {
+      routes.push({
+        path: '*',
+        component: resolve(__dirname, 'pages/404.vue'),
+      })
+    },
+  },
   css: [],
   plugins: [],
   styleResources: {
