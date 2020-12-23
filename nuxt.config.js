690ff7c8b58fbb648b122a40435e3b9fd088e961
larsmyrup <hej@larsmyrup.dk>
2020-12-23 20:20:21 +0100

    Added 404 page

--- nuxt.config.js
+++ nuxt.config.js
@@ -11,14 +11,6 @@ export default {
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
     ],
   },
-  router: {
-    extendRoutes(routes, resolve) {
-      routes.push({
-        path: '*',
-        component: resolve(__dirname, 'pages/index.vue'),
-      })
-    },
-  },
   css: [],
   plugins: [],
   styleResources: {
