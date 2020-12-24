036f3cb383356d030c8dd0d41a1b2c7c5de6924e
larsmyrup <hej@larsmyrup.dk>
2020-12-24 11:37:18 +0100

    Adding meta cover

--- nuxt.config.js
+++ nuxt.config.js
@@ -6,6 +6,11 @@ export default {
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: '' },
+      {
+        hid: 'og:image',
+        property: 'og:image',
+        content: '/tsucover.jpg',
+      },
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
